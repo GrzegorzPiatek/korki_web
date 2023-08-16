52fb5aaf713293afdc2b8fa30ab7efb14ddd9437
Grzegorz <grzegorz.piatek@scan-it.pl>
2023-08-16 16:24:41 +0200

    Add some OOP concept

---
+++ Basics/oop.js
@@ -0,0 +1,16 @@
+class User {
+    name;
+    surname;
+    password;
+    changePassword(newPass);
+}
+
+let adam = new User();
+let jan = new User();
+let kacper = new User();
+jan.name = "ads"
+jan.surname = "ads"
+jan.changePassword()
+jan.secondName = 0;
+
+console.log(jan.secondName)
